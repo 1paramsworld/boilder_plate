ac48263cd1ef9d5f638f30250b815f6b6d6563f7
Param Shah <param.sanjay.shah@gmail.com>
2024-03-07 11:05:19 +0530

    all added

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const uipath = path.join(__dirname,"/templates");
+app.set("view engine", "hbs");
+app.set("views", uipath);
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+app.listen(3000)
